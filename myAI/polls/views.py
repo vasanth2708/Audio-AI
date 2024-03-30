e5b687478caa4c3147dd5160cf63e90a091cf5c8
Vasanth-Gandham <114437061+vasanth2708@users.noreply.github.com>
2024-03-29 23:24:59 -0700

    Add files via upload

--- myAI/polls/views.py
+++ myAI/polls/views.py
@@ -3,60 +3,86 @@ from django.template import loader, RequestContext
 from django.http import HttpResponse, JsonResponse
 from openai import OpenAI
 import os
+import asyncio
+from deepgram import Deepgram
+from myAI.settings import BASE_DIR
+
+
+dp_key = "" 
+dg = Deepgram(dp_key)
+
+params = {
+    'diarize': True,
+    'punctuate': True, 
+    'model': 'general', 
+}
+async def process_audio(file_path):
+    dict = {}
+    with open(file_path, "rb") as f:
+        source = {"buffer": f, "mimetype": 'audio/mp4'}
+        response = await dg.transcription.prerecorded(source, params)
+        if 'results' in response and 'channels' in response['results']:
+            for channel in response['results']['channels']:
+                channel_id = channel.get('channel', 'Unknown channel')
+                print(f"Channel {channel_id}:")
+                for alternative in channel.get('alternatives', []):
+                    for word in alternative.get('words', []):
+                        speaker = word.get('speaker', 'Unknown speaker')
+                        if(speaker in dict):
+                            dict[speaker].append(word['word'])
+                        else:
+                            dict[speaker]=[word['word']]
+        else:
+            print("No valid 'results' or 'channels' data found in the response.")
+    return dict
 
 def home(request):
     return render(request, 'index.html', {'what':'Welcome To Text AI..'})
 def upload(request):
     context = {}
     if request.method == 'POST':
-        uploaded_file = request.FILES['file']  # Get the uploaded file
+        uploaded_file = request.FILES['file']
         filename = str(uploaded_file)
-        dialogues = handle_uploaded_file(uploaded_file, filename)
+        dialogues = handle_uploaded_file(uploaded_file,filename)
         context['dialogues'] = dialogues
         context['success'] = True
     else:
         context['success'] = False
     return render(request, 'result.html', context)
 
-def handle_uploaded_file(uploaded_file, filename):
-    upload_dir = 'upload/'
+def handle_uploaded_file(uploaded_file,filename):
+    upload_dir = 'uploads'
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
-    with open(os.path.join(upload_dir, filename), 'wb+') as destination:
+    file_path = os.path.join(upload_dir, filename)
+    with open(file_path, 'wb+') as destination:
         for chunk in uploaded_file.chunks():
             destination.write(chunk)
-    dialogues = parse_dialogues(os.path.join(upload_dir, filename))
-    return dialogues
+    textS = asyncio.run(process_audio(file_path))
+    for k in textS:
+        textS[k] = " ".join(textS[k])
+    print(textS)
+    return parse_dialogues(textS)
 
 def parse_dialogues(file_path):
-    dialogues = {}
-    with open(file_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            parts = line.split(':', 1)
-            if len(parts) == 2:
-                speaker, dialogue = parts[0].strip(), parts[1].strip()
-                if speaker not in dialogues:
-                    dialogues[speaker] = []
-                dialogues[speaker].append(dialogue)
-    return openAi_Results(dialogues)
+    return openAi_Results(file_path)
 
 def openAi_Results(dialogues):
     openai_key = ""
     client = OpenAI(api_key=openai_key)
     analysis =  {}
     for v in dialogues:
-        for i in dialogues[v]:
-            completion =client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role":"system","content":"Act as sentimental analysis in 3words"},
-                {"role": "user", "content": f"{i}"}
-            ]
-            )
-            an = completion.choices[0].message.content.strip()
-            if(v in analysis):
-                analysis[v].append(an)
-            else:
-                analysis[v]=[an]
+        completion =client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role":"system","content":"Act as sentimental analysis"},
+            {"role": "user", "content": f"{dialogues[v]}"}
+        ]
+        )
+        an = completion.choices[0].message.content.strip()
+        if(v in analysis):
+            analysis[v].append(an)
+        else:
+            analysis[v]=[an]
     return analysis
     
